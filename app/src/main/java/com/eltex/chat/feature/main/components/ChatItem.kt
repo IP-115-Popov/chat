c3f0c6ed06cbd15afb17d8c234139d86eea046b0
Sergey Popov <serzh.serp123.popov@mail.ru>
2025-02-09 15:25:34 +0700

    feat: Разметка ChatItem добавил галочки и статус

--- app/src/main/java/com/eltex/chat/feature/main/components/ChatItem.kt
+++ app/src/main/java/com/eltex/chat/feature/main/components/ChatItem.kt
@@ -12,13 +12,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.eltex.chat.R
+import com.eltex.chat.feature.main.viewmodel.uistate.MessageStatus
 import com.eltex.chat.ui.theme.CustomTheme
 
 @Composable
@@ -27,7 +32,7 @@ fun ChatItem(
     title: String,
     message: String,
     time: String,
-    unreadableCount: Int,
+    messageStatus: MessageStatus
 ) {
     Row(
         modifier = Modifier
@@ -66,11 +71,33 @@ fun ChatItem(
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
                 )
-                Text(
-                    text = time,
-                    style = CustomTheme.typographySfPro.caption1Regular,
-                    color = CustomTheme.basicPalette.grey,
-                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    when (messageStatus) {
+                        MessageStatus.yourLastMessageIsNotRead -> {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(R.drawable.ic_done),
+                                contentDescription = null,
+                                tint = CustomTheme.basicPalette.lightBlue
+                            )
+                        }
+                        MessageStatus.yourLastMessageIsRead -> {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(R.drawable.ic_done_all),
+                                contentDescription = null,
+                                tint = CustomTheme.basicPalette.lightBlue
+                            )
+                        }
+                        else -> {}
+                    }
+                    Spacer(Modifier.size(2.dp))
+                    Text(
+                        text = time,
+                        style = CustomTheme.typographySfPro.caption1Regular,
+                        color = CustomTheme.basicPalette.grey,
+                    )
+                }
             }
             Row(
                 modifier = Modifier
@@ -87,18 +114,21 @@ fun ChatItem(
                     overflow = TextOverflow.Ellipsis
                 )
                 Spacer(Modifier.size(41.dp))
-                Box(
-                    modifier = Modifier
-
-                        .size(16.dp)
-                        .background(CustomTheme.basicPalette.lightBlue, shape = CircleShape),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = if (unreadableCount < 10) unreadableCount.toString() else "...",
-                        style = CustomTheme.typographySfPro.caption2Regular,
-                        color = CustomTheme.basicPalette.white,
-                    )
+                if (messageStatus is MessageStatus.missedMessages && messageStatus.count > 0) {
+                    Box(
+                        modifier = Modifier
+                            .size(16.dp)
+                            .background(CustomTheme.basicPalette.lightBlue, shape = CircleShape),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = if (messageStatus.count < 10) messageStatus.count.toString() else "...",
+                            style = CustomTheme.typographySfPro.caption2Regular,
+                            color = CustomTheme.basicPalette.white,
+                        )
+                    }
+                } else {
+                    Spacer(Modifier.size(16.dp))
                 }
             }
         }
@@ -111,7 +141,7 @@ fun ChatItemPreview() {
     CustomTheme {
         ChatItem(
             imageText = "ФИ", title = "Избранное", message = "Текст сообщения", time = "15:30",
-            unreadableCount = 1,
+            messageStatus = MessageStatus.missedMessages(9)
         )
     }
 }
@@ -125,7 +155,7 @@ fun ChatItemPreview2() {
             title = "ИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранное",
             message = "ТекстсообщенияТекстсообщенияТекстсообщениТекстсообщенияТекстсообщенияТекстсообщения",
             time = "15:30",
-            unreadableCount = 9,
+            messageStatus = MessageStatus.missedMessages(0)
         )
     }
 }
@@ -139,7 +169,33 @@ fun ChatItemPreview3() {
             title = "ИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранное",
             message = "ТекстсообщенияТекстсообщенияТекстсообщениТекстсообщенияТекстсообщенияТекстсообщения",
             time = "15:30",
-            unreadableCount = 100,
+            messageStatus = MessageStatus.yourLastMessageIsRead
+        )
+    }
+}
+@Preview
+@Composable
+fun ChatItemPreview4() {
+    CustomTheme {
+        ChatItem(
+            imageText = "ФИ",
+            title = "ИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранное",
+            message = "ТекстсообщенияТекстсообщенияТекстсообщениТекстсообщенияТекстсообщенияТекстсообщения",
+            time = "Вчера",
+            messageStatus = MessageStatus.yourLastMessageIsNotRead
+        )
+    }
+}
+@Preview
+@Composable
+fun ChatItemPreview5() {
+    CustomTheme {
+        ChatItem(
+            imageText = "ФИ",
+            title = "ИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранноеИзбранное",
+            message = "ТекстсообщенияТекстсообщенияТекстсообщениТекстсообщенияТекстсообщенияТекстсообщения",
+            time = "Пн",
+            messageStatus = MessageStatus.yourLastMessageIsNotRead
         )
     }
 }
---
+++ app/src/main/java/com/eltex/chat/feature/main/viewmodel/uistate/MessageStatus.kt
@@ -0,0 +1,7 @@
+package com.eltex.chat.feature.main.viewmodel.uistate
+
+sealed interface MessageStatus {
+    object yourLastMessageIsRead: MessageStatus
+    object yourLastMessageIsNotRead: MessageStatus
+    class missedMessages(val count: Int): MessageStatus
+}
