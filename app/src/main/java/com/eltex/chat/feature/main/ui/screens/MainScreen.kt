0407ee55cf296e10ff1b40902e3589804bfcb5f1
попов Сергей <serzh.serp123.popov@mail.ru>
2025-02-11 14:44:58 +0700

    feat: Разметка MainScreen

--- app/src/main/java/com/eltex/chat/feature/main/ui/screens/MainScreen.kt
+++ app/src/main/java/com/eltex/chat/feature/main/ui/screens/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.eltex.chat.feature.main.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,24 +9,41 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.eltex.chat.R
-import com.eltex.chat.feature.main.ui.components.MainScreenTopBar
 import com.eltex.chat.feature.main.ui.components.SearchField
 import com.eltex.chat.ui.theme.CustomTheme
 
 @Composable
 fun MainScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
-        Scaffold(topBar = { MainScreenTopBar(onAddClick = {}) }) { innerPadding ->
+            Box(Modifier.height(48.dp).fillMaxWidth().background(CustomTheme.basicPalette.blue))
+            Box(Modifier.height(44.dp).fillMaxWidth().background(CustomTheme.basicPalette.blue)){
+                Text(
+                    text = stringResource(R.string.chats),
+                    style = CustomTheme.typographySfPro.titleMedium,
+                    modifier = Modifier.align(Alignment.Center),
+                    color = CustomTheme.basicPalette.white
+                )
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_add),
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterEnd).padding(end = 16.dp).clickable {  },
+                    tint = CustomTheme.basicPalette.white
+                )
+
+            }
             Box(
                 modifier = Modifier
-                    .padding(innerPadding)
                     .height(44.dp)
                     .fillMaxWidth()
                     .background(CustomTheme.basicPalette.blue)
@@ -41,7 +59,6 @@ fun MainScreen() {
             Column {
                 LazyColumn(
                     modifier = Modifier
-                        .padding(innerPadding)
                         .fillMaxSize()
                 ) {
 //                items() {
@@ -50,10 +67,9 @@ fun MainScreen() {
                 }
             }
         }
-    }
 }
 
-@Preview(showSystemUi = false)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun MainScreenPreview() {
     CustomTheme {
