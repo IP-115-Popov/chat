6710c3dba8ebd4176ca2878a121e525b4c316a7f
попов Сергей <serzh.serp123.popov@mail.ru>
2025-02-20 14:42:59 +0700

    feat: Улучшил вывод инициалов

--- app/src/main/java/com/eltex/chat/utils/StringExtension.kt
+++ app/src/main/java/com/eltex/chat/utils/StringExtension.kt
@@ -4,6 +4,7 @@ fun String.getInitials(): String {
     return this.split(" ")
         .mapNotNull {
             it.firstOrNull()?.toString()
-        } // Получаем первую букву каждого слова, обрабатываем null
-        .joinToString("") // Объединяем буквы в строку
+        }
+        .joinToString("")
+        .toUpperCase()
 }
