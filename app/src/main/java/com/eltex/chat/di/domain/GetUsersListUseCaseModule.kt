acb2313bb212af719a46e1d04e57f4505e15082c
попов Сергей <serzh.serp123.popov@mail.ru>
2025-03-02 21:02:37 +0700

    fix: GetUsersListUseCase Предотвращение дудоса и поломки. Кеширование, с обновлением кеша, при следующем запросе, позже 15 секунд.

--- app/src/main/java/com/eltex/chat/di/domain/GetUsersListUseCaseModule.kt
+++ app/src/main/java/com/eltex/chat/di/domain/GetUsersListUseCaseModule.kt
@@ -6,10 +6,13 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 class GetUsersListUseCaseModule {
+    @Singleton
     @Provides
     fun provideGetUsersListUseCase(
         usersRemoteRepository: UsersRemoteRepository,
--- domain/src/main/java/com/eltex/domain/usecase/remote/GetUsersListUseCase.kt
+++ domain/src/main/java/com/eltex/domain/usecase/remote/GetUsersListUseCase.kt
@@ -4,33 +4,67 @@ import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
 import com.eltex.domain.models.DataError
+import com.eltex.domain.models.ProfileModel
 import com.eltex.domain.models.UserModel
 import com.eltex.domain.repository.remote.UsersRemoteRepository
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import java.time.Duration
+import java.time.Instant
 
 class GetUsersListUseCase(
     private val usersRemoteRepository: UsersRemoteRepository,
 ) {
+    private data class CachedData(
+        val userList: List<UserModel>,
+        val timestamp: Instant
+    )
+
+    private var cachedData: CachedData? = null
+    private val mutex = Mutex()
+    private val cacheExpirationDuration = Duration.ofSeconds(15)
+
     suspend operator fun invoke(
         query: String
     ): Either<DataError, List<UserModel>> {
-        val usersResult = usersRemoteRepository.getUsersList(
-            query = query,
-            count = 100000,
-            offset = 0,
-        )
-
-        return when (usersResult) {
-            is Either.Left -> usersResult
-            is Either.Right -> {
-                val users = usersResult.value.filter {
-                    it.name.contains(query, ignoreCase = true) || it.username.contains(
-                        query,
-                        ignoreCase = true
-                    )
+        mutex.withLock {
+            val currentCachedData = cachedData
+            if (currentCachedData != null &&
+                Instant.now().isBefore(currentCachedData.timestamp.plus(cacheExpirationDuration))
+            ) {
+                val filteredList = currentCachedData.userList.filter {
+                    it.name.contains(query, ignoreCase = true) ||
+                            it.username.contains(query, ignoreCase = true)
                 }
-                if (users.isNotEmpty()) users.right()
-                else DataError.DefaultError.left()
+                if (filteredList.isNotEmpty()) {
+                    return Either.Right(filteredList)
+                }
             }
         }
+
+            val usersResult = usersRemoteRepository.getUsersList(
+                query = query,
+                count = 100000,
+                offset = 0,
+            )
+
+            return when (usersResult) {
+                is Either.Left -> usersResult
+                is Either.Right -> {
+                    val users = usersResult.value.filter {
+                        it.name.contains(query, ignoreCase = true) || it.username.contains(
+                            query,
+                            ignoreCase = true
+                        )
+                    }
+                    mutex.withLock {
+                        if (users.isNotEmpty()) {
+                            cachedData = CachedData(users, Instant.now())
+                            users.right()
+                        }
+                        else DataError.DefaultError.left()
+                    }
+                }
+            }
     }
 }
